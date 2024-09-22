9e132a7b5fb3f3e0f0fed8f9e9b69f51055e968f
qaswerg <khako@qaswerg>
2024-09-23 00:55:15 +0300

    error

--- show_meme.py
+++ show_meme.py
@@ -2,7 +2,7 @@ from PIL import Image
 import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
-    img = Image.open('memes/mem2.jpg')
+    img = Image.open('memes/mem2adfs.jpg')
 
     plt.imshow(img)
     plt.axis('off')
