4beec10eb7cef8323688de114fb3414466779fbc
qaswerg <khako@qaswerg>
2024-09-23 00:53:01 +0300

    mem 1

---
+++ show_meme.py
@@ -0,0 +1,9 @@
+from PIL import Image
+import matplotlib.pyplot as plt
+
+if __name__ == "__main__":
+    img = Image.open('memes/mem1.jpg')
+
+    plt.imshow(img)
+    plt.axis('off')
+    plt.show()
